a4a8453a14a08c80cb7060cc20b41725d0829c79
zhiriezra <zhiriezra@gmail.com>
2020-10-21 14:10:48 +0100

    2020 newsapp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,50 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  theNation: string = 'http://thenationonlineng.net/category/health';
+  leadership: string = 'https://leadership.ng/category/news/health';
+  tribune: string = 'https://www.tribuneonlineng.com/health';
+  dailyTimes: string = 'https://dailytimes.ng/category/health';
+  nationalMirror: string = 'https://www.nationalmirroronline.net/health';
 
+  constructor(private iab: InAppBrowser) {}
+
+  openTheNation(){
+    const options: InAppBrowserOptions = {
+      zoom: 'no'
+    }
+    this.iab.create(this.theNation, '_blank');
+  }
+
+  openTheLeadership() {
+    const options: InAppBrowserOptions = {
+  		zoom: 'no'
+  	}
+    
+    this.iab.create(this.leadership, '_blank');
+  }
+
+  openTribune() {
+    const options: InAppBrowserOptions = {
+  		zoom: 'no'
+  	}
+
+    this.iab.create(this.tribune, '_blank');
+  }
+
+  openDailyTimes() {
+    const options: InAppBrowserOptions = {
+  		zoom: 'no'
+  	}
+
+    this.iab.create(this.dailyTimes, '_blank');
+  }
+
+  openNationalMirror() {
+    const options: InAppBrowserOptions = {
+  		zoom: 'no'
+  	}
+
+    this.iab.create(this.nationalMirror, '_blank');
+  }
 }
